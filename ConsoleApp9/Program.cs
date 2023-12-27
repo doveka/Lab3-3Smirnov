c9d33bdfc92c6218831c5ce54f7a2942dc0a5170
doveka <131176206+doveka@users.noreply.github.com>
2023-12-27 06:39:14 +0200

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.Write("Введите x:");    
+    double x=int.Parse(Console.ReadLine());
+    double s = 0;
+    for (int i = 1; i <= x; i++)
+        s += Math.Pow(x, 3) / 3 + Math.Pow(x, 5) / 15 + Math.Pow(x, 7) / 35 / i;
+    Console.Write($"S={s:F2}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+// не смог разобраться, но вроде так :(((
